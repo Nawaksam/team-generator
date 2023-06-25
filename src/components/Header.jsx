c389c2c6dbb5248320497e983770264920cd4836
Nawaksam <sam.cardonnel@gmail.com>
2023-06-25 13:31:32 +0200

    testing fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <div className="bg-zinc-800 text-emerald-500 flex sm:flex-row flex-col sm:justify-between items-center py-1 px-6 ">
-      <h1 className=" text-4xl sm:py-0 py-2">CodeBakery</h1>
-      <h2 className="text-4xl sm:text-left text-center sm:py-0 py-3 sm:border-none border-y-2 border-emerald-900">
+    <div className="bg-zinc-800 text-emerald-500 flex sm:flex-row flex-col sm:justify-between items-center py-1 px-6 font-major text-4xl">
+      <h1 className="sm:py-0 py-2">CodeBakery</h1>
+      <h2 className=" text-center sm:py-0 py-3 sm:border-none border-y-2 border-emerald-900">
         Victim picker & Team generator
       </h2>
       <img
