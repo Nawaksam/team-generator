3c7c5431a5aa64a8579aefd55fdb0dc454d9dde3
Nawaksam <sam.cardonnel@gmail.com>
2023-06-25 13:33:12 +0200

    fonts bis

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <div className="bg-zinc-800 text-emerald-500 flex sm:flex-row flex-col sm:justify-between items-center py-1 px-6 font-major text-4xl">
-      <h1 className="sm:py-0 py-2">CodeBakery</h1>
+      <h1 className="sm:py-0 py-2 sm:pr-4">CodeBakery</h1>
       <h2 className=" text-center sm:py-0 py-3 sm:border-none border-y-2 border-emerald-900">
-        Victim picker & Team generator
+        Victim Picker & Team Generator
       </h2>
       <img
         src="/baked.png"
